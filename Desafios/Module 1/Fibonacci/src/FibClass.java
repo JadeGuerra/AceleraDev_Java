97b39ebf9f428091052192c276fd405dcafd3f39
JadeGuerra <jug.guerra@gmail.com>
2020-06-17 17:51:58 -0300

    organizando

---
+++ Desafios/Module 1/Fibonacci/src/FibClass.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FibClass {
+    //cria uma ArrayList @fibonacci
+    public static List<Integer> fibonacci () {
+        List<Integer> listFibonacci = new ArrayList<Integer>();
+        //inicializa @listFibonacci
+        listFibonacci.add(0);
+        listFibonacci.add(1);
+
+        //inicializa @numeroFibonacci
+        int numeroFibonacci = 1;
+
+        //adiciona o restante dos números na @listFibonacci
+        do {
+            numeroFibonacci += listFibonacci.get(listFibonacci.size() - 2);
+            listFibonacci.add(numeroFibonacci);
+        }
+        while (listFibonacci.size() + 1 <= 350);
+
+        //retorna a lista completa
+        return listFibonacci;
+    }
+
+    //Consulta se @a está em @fibonacci
+    public static Boolean isFibonacci(Integer a) {
+        return fibonacci().contains(a);
+    }
+
+}
---
+++ Desafios/Module 1/Fibonacci/src/Fibonacci.java
@@ -0,0 +1,23 @@
+/**
+ * Faça um método @Desafio("Fibonacci") que retorne
+ * uma lista java.util.List com os primeiro números
+ * Integer da série de Fibonacci até passar de 350.
+ * Faça um segundo método @Desafio("isFibonnaci")
+ * que recebe um Integer como parâmetro e retorne
+ * Boolean. Caso o número recebido por
+ * parâmetro esteja entre os números da sequência de Fibonnaci
+ * do @Desafio("Fibonnaci") retorne true, senão, false.
+ **/
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Digite o número para consultar: ");
+        int a = s.nextInt();
+        FibClass fibClass = new FibClass();
+        fibClass.isFibonacci(a);
+        System.out.println(fibClass);
+
+    }
