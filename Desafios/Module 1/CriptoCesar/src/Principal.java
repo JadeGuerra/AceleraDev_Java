e7aef31c5c4521dec1c59033cedc6f2e24fcea21
JadeGuerra <jug.guerra@gmail.com>
2020-06-19 14:18:44 -0300

    estruturando impressão

--- Desafios/Module 1/CriptoCesar/src/Principal.java
+++ Desafios/Module 1/CriptoCesar/src/Principal.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 public class Principal {//all mine
 
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
         System.out.print("Escreva o texto: ");
         String texto = s.nextLine();
-        System.out.
-
+        System.out.println("Para criptografar digite 1 ");
+        System.out.println("Para descriptografar digite 2 ");
+        byte definicao = s.nextByte();
         Criptografia criptografia = new CriptografiaCesariana();
-        String criptografado = criptografia.criptografar(texto);
-        String descriptografado = criptografia.descriptografar(texto);
-        System.out.println();
+
+        if (definicao == 1 ) {
+            String criptografado = criptografia.criptografar(texto);
+            System.out.println(criptografado);
+        }else if (definicao == 2){
+            String descriptografado = criptografia.descriptografar(texto);
+            System.out.println(descriptografado);
+        } else {
+            System.out.println("Nenhuma ação selecionada");
+        }
+
+    }
+
 
 }
