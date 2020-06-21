8f0d1bc012b9cb27651725bc1404b5745ce90d42
JadeGuerra <jug.guerra@gmail.com>
2020-06-21 18:48:08 -0300

    calculadora pronta

---
+++ Desafios/Module 1/CalcSalarioLiquido/src/Principal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Principal {//all mine
+    public static void main(String[] args) {
+        System.out.println("Calculadora salário líquido");
+        System.out.println("Digite o valor do salário usando ponto para as casas decimais:");
+        Scanner s = new Scanner(System.in);
+        String salario = s.nextLine();
+        double salarioBase = Double.parseDouble(salario);
+
+        double salarioLiquido = CalculadoraSalario.calcularSalarioLiquido(salarioBase);
+
+        System.out.println(String.valueOf(salarioLiquido));
+
+
+    }
+}
