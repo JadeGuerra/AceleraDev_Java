bf37136a96d0bb7515476931029f421514c352c5
JadeGuerra <jug.guerra@gmail.com>
2020-06-23 19:24:12 -0300

    incluir jogador construido

--- Desafios/Module 2/GerTimesFutebol/src/codenation/Jogador.java
+++ Desafios/Module 2/GerTimesFutebol/src/codenation/Jogador.java
@@ -12,20 +12,22 @@ public class Jogador {
     private Integer nivelHabilidade;
     private BigDecimal salario;
     private boolean capitao = false;
-    //contrutor
+    //construtor
 
     public void jogador (Long id,
                          Long idTime,
                          String nome,
                          LocalDate dataNascimento,
                          Integer nivelHabilidade,
-                         BigDecimal salario){
+                         BigDecimal salario,
+                         boolean capitao){
         this.id = id;
         this.idTime = idTime;
         this.nome = nome;
         this.dataNascimento = dataNascimento;
         this.nivelHabilidade = nivelHabilidade;
         this.salario = salario;
+        this.capitao = capitao;
     }
     //setters getters
 
